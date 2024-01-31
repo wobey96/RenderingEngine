219a11d454cf7f6b9f77fd3ee162dfa2b8ec0c97
Wallace Obey <wobey96@gmail.com>
2024-01-31 05:36:05 +0000

    getting cmake setup before vulkan stuff

---
+++ build/CMakeFiles/RenderingEngineVK.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RenderingEngineVK.dir/main.cpp.o"
+  "RenderingEngineVK"
+  "RenderingEngineVK.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RenderingEngineVK.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
